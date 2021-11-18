9817728c99bd76b87fb6dd11d361f2768c6a54ba
Olowu Abayomi <aayobam@gmail.com>
2021-11-18 14:54:31 +0100

    Delete MyApi directory

--- MyApi/secondapp/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Car
-
-
-@admin.register(Car)
-class AdminCar(admin.ModelAdmin):
-    list_display = ('car_brand', 'car_model',
-                    'production_year', 'car_body', 'engine_type')
