0fc941ec063b0984518494dfd64a7b8e3834251f
Olowu Abayomi <aayobam@gmail.com>
2022-02-15 17:55:32 +0100

    update

--- todo_rest_api/urls.py
+++ todo_rest_api/urls.py
@@ -6,6 +6,7 @@ from drf_yasg.views import get_schema_view
 
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 schema_view = get_schema_view(
    openapi.Info(
@@ -32,4 +33,5 @@ urlpatterns = [
     path('swagger.json', schema_view.without_ui(cache_timeout=0), name='schema-json'),
 ]
 
+urlpatterns += staticfiles_urlpatterns()
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
